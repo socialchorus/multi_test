4a33ec086f0b17612c07ccbda7946ef4aab8f90f
Matt Wynne <matt@mattwynne.net>
2013-05-20 14:12:16 +0100

    Deliberately break it to test Travi

--- lib/multi_test.rb
+++ lib/multi_test.rb
@@ -1,7 +1,7 @@
 module MultiTest
   def self.disable_autorun
     if defined?(Test::Unit::Runner)
-      Test::Unit::Runner.module_eval("@@stop_auto_run = true")
+      #Test::Unit::Runner.module_eval("@@stop_auto_run = true")
     end
   end
 end
